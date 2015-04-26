a7251bf56843186b4524c23b21a90496394c5bf4
6357 <c6357@outlook.com>
2015-04-26 22:02:12 +0800

    add share

---
+++ YUAppShare/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  YUAppShare
+//
+//  Created by yuzhx on 15/4/26.
+//  Copyright (c) 2015年 BruceYu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
